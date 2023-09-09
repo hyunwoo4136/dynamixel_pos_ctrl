ec0903a9f578c66133b177593e1c2bff9ccdd4b0
Hyunwoo Lee <hyunwoo745@naver.com>
2023-09-09 22:51:47 +0900

    position cmd subscriber added

--- src/dynamixel_pos_ctrl.cpp
+++ src/dynamixel_pos_ctrl.cpp
@@ -1,9 +1,21 @@
 #include <ros/ros.h>
-#include "std_msgs/Bool.h"
+#include "std_msgs/Int16.h"
 #include "dynamixel_workbench_msgs/DynamixelCommand.h"
 
 
 ///////////////////////////////////////////////////////////////////////////	var. declaration
+bool send_flag=false;
+
+int pos;
+
+
+///////////////////////////////////////////////////////////////////////////	cmd call back func.
+void cmd_callback(const std_msgs::Int16::ConstPtr& msg)
+{
+	send_flag=true;
+	
+	pos=msg->data;
+}
 
 
 ///////////////////////////////////////////////////////////////////////////	main function
@@ -14,23 +26,29 @@ int main(int argc, char **argv)
 	
 	ros::Rate loop_rate(5);
 	
-	ros::ServiceClient client_cmd;
+	ros::Subscriber cmd_sub;							// subscriber declaration
+	cmd_sub=nh.subscribe("cmd_pos", 10, cmd_callback);
+	
+	ros::ServiceClient client_cmd;						// service client declaration
 	client_cmd=nh.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("/dynamixel_workbench/dynamixel_command");
 	
-	dynamixel_workbench_msgs::DynamixelCommand cmd;
+	dynamixel_workbench_msgs::DynamixelCommand cmd;		// service request declaration
 	
 	cmd.request.command="";
 	cmd.request.id=0;
 	cmd.request.addr_name="Goal_Position";
-	cmd.request.value=3072;
-	
-	client_cmd.call(cmd);
 	
 	while(ros::ok())
 	{
 		ros::spinOnce();
 		
-		client_cmd.call(cmd);
+		if(send_flag==true)
+		{
+			cmd.request.value=pos;
+			client_cmd.call(cmd);
+			
+			send_flag=false;
+		}
 		
 		loop_rate.sleep();
 	}
